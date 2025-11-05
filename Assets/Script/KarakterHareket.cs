fc188dad7a49af42b04bcd6acb2efed86b105962
Alp Eren Akbaş <86376372+IBullseyea@users.noreply.github.com>
2025-11-05 21:28:17 +0300

    Basic character checks have been performed.

---
+++ Assets/Script/KarakterHareket.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+
+public class KarakterHareket : MonoBehaviour
+{
+    //hareket için gerekli değişkenler
+    public float hareketHizi = 5f;
+    private float yatayInput;
+
+    //zıplama için gerekli değişkenler
+    public float ziplamaGucu = 10f;
+    private bool yerdeMi;
+
+    //zemin kontrolü için gerekli değişkenler
+    public Transform zeminKontrolNoktasi; //zemin kontrol noktası
+    public LayerMask zeminLayer; //zemin katmanı
+
+    private Rigidbody2D rb;// Karakterin fizik bileşeni
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>(); 
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //sağ sol hareketi için yatay input al
+        yatayInput = Input.GetAxis("Horizontal");
+
+
+        //zeminde olup olmadığını kontrol et
+        yerdeMi = Physics2D.OverlapCircle(zeminKontrolNoktasi.position, 0.1f, zeminLayer);
+
+        //zıplama işlemi
+        if (yerdeMi == true && Input.GetKeyDown(KeyCode.Space))
+        {
+            // Rigidbody'nin dikey hızını (velocity.y) anında zıplama gücüne eşitle.
+            // (Mevcut yatay hızı (velocity.x) koru)
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, ziplamaGucu);
+        }
+
+
+
+
+        Vector2 yeniHiz = new Vector2(yatayInput * hareketHizi, rb.linearVelocity.y);
+        rb.linearVelocity = yeniHiz;
+
+       
+
+
+    }
+
+    void FixedUpdate()
+    {
+
+        // HAREKETİ FİZİKSEL OLARAK UYGULA
+        // Rigidbody'nin yatay hızını (velocity.x) hesapla
+        // (Dikey hızı (velocity.y) koru, zıplamayı bozmasın)
+        rb.linearVelocity = new Vector2(yatayInput * hareketHizi, rb.linearVelocity.y);
+    }
+}
